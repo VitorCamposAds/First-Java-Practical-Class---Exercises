1bc9bb5a9b97b3245f5b02b9608113be2b0a3a1b
Vitor_IT <121561704+VitorCamposAds@users.noreply.github.com>
2023-06-27 14:40:05 -0300

    Reading and Printing Names in Reverse Order - Java
    This Java program reads in a number from the user via the Scanner class and uses it to determine how many names to read in from the user. It then initializes an ArrayList of strings and adds each name to the list using the add method. Finally, the program prints out the list of names in the order they were entered, and then prints out the list in reverse order using a for loop that iterates through the list in reverse order. The teclado.nextLine() method is used to consume the newline character after reading in the integer value to avoid any issues with reading in the names. This program can be useful for practicing input/output and working with ArrayLists in Java.

--- Principal.java
+++ Principal.java
@@ -1,30 +1,32 @@
 package empresa;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Principal {
 
-    public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-        int palpite;
-        int valorCorreto = 10_000; // Corrected the syntax for assigning a value to an int variable
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		ArrayList<String> ListaNomes = new ArrayList<String>();
+		System.out.println("Digite a quantidade de nomes: ");
+		int qtd = teclado.nextInt();
+		teclado.nextLine(); // consume newline character
+		String nome;
+		for(int i=0; i<qtd; i++) {
+			System.out.println("Digite o nome " + (i+1) + ":");
+			nome = teclado.nextLine();
+			ListaNomes.add(nome);
+		}
+		
+		System.out.println("Nomes digitados:");
+		for(String n : ListaNomes) {
+			System.out.println(n);
+		}
+		
+		for(int i=ListaNomes.size()-1; i>=0;i--)
+			System.out.println("Os nomes de trás para frente são:" + ListaNomes.get(i));		
+		
+	}
 
-        System.out.println("Qual o seu palpite? ");
-        palpite = teclado.nextInt();
-
-        while (palpite != valorCorreto) {
-            if (palpite > valorCorreto) {
-                System.out.println("Seu palpite está acima do número correto!");
-            } 
-            
-            else {
-                	System.out.println("Seu palpite está abaixo do número correto!");
-            }
-            System.out.println("Digite outro palpite:  "); // Added this line to ask for input again
-            palpite = teclado.nextInt();
-        }
-
-        System.out.println("Você acertou!");
-    }
 }
-
