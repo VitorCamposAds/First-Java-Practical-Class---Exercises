16e1db7e64cb51b1c10e80e3109f6ef6fa88d63a
Vitor_IT <121561704+VitorCamposAds@users.noreply.github.com>
2023-06-27 14:58:57 -0300

    Java Program to Calculate BMI (Body Mass Index)
    This is a Java program that calculates the BMI (Body Mass Index) of a person based on their weight and height. The program prompts the user to enter their weight and height, reads in the values using the Scanner class, and then calculates the BMI using the formula BMI = weight / (height * height). The program then prints out the calculated BMI value using the printf method with a precision of two decimal places. The program can be useful for anyone who wants to calculate their BMI in a simple and efficient way using Java programming language.

--- Principal.java
+++ Principal.java
@@ -1,32 +1,19 @@
 package empresa;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Principal {
 
 	public static void main(String[] args) {
+		System.out.println("Digite peso seguido de altura!");
 		Scanner teclado = new Scanner(System.in);
+		int peso = teclado.nextInt();
+		double altura = teclado.nextDouble();
 		
-		ArrayList<String> ListaNomes = new ArrayList<String>();
-		System.out.println("Digite a quantidade de nomes: ");
-		int qtd = teclado.nextInt();
-		teclado.nextLine(); // consume newline character
-		String nome;
-		for(int i=0; i<qtd; i++) {
-			System.out.println("Digite o nome " + (i+1) + ":");
-			nome = teclado.nextLine();
-			ListaNomes.add(nome);
-		}
+		double imc = peso / (altura * altura);
+		System.out.printf("Seu IMC é: %.2f", imc);
 		
-		System.out.println("Nomes digitados:");
-		for(String n : ListaNomes) {
-			System.out.println(n);
-		}
-		
-		for(int i=ListaNomes.size()-1; i>=0;i--)
-			System.out.println("Os nomes de trás para frente são:" + ListaNomes.get(i));		
-		
+
 	}
 
 }
